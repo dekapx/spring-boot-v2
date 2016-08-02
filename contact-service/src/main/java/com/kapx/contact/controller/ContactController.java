413ed36a80923cb7990c80e97cd778588939c10d
KAPIL <xkapil@gmail.com>
2016-08-02 11:44:38 +0100

    controller modified

--- contact-service/src/main/java/com/kapx/contact/controller/ContactController.java
+++ contact-service/src/main/java/com/kapx/contact/controller/ContactController.java
@@ -1,19 +1,29 @@
 package com.kapx.contact.controller;
 
+import com.kapx.contact.domain.Contact;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ContactController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ContactController.class);
 
-    @RequestMapping("/ping")
+    @RequestMapping(value = "/contact/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+    public Contact getContact() {
+        return null;
+    }
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public String ping() {
         LOGGER.info("--- Contact Service REST Controller ping method invoked. ---");
         return "Contact Service REST Controller";
     }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
 }
