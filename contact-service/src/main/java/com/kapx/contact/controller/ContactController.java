198607b81f880fe104b74dd0acba2298caf50b1a
KAPIL <xkapil@gmail.com>
2016-08-03 15:52:51 +0100

    code refactoring

--- contact-service/src/main/java/com/kapx/contact/controller/ContactController.java
+++ contact-service/src/main/java/com/kapx/contact/controller/ContactController.java
@@ -3,14 +3,16 @@ package com.kapx.contact.controller;
 import com.kapx.contact.vo.ContactVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class ContactController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ContactController.class);
 
-    @RequestMapping(value = "/contact/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+    @RequestMapping(value = "/contact/{id}", method = GET)
     public ContactVO getContact(@PathVariable final Long id) {
         LOGGER.info("--- getContact REST requestfor ID {} ---", id);
         final ContactVO contactVO = new ContactVO.ContactBuilder()
@@ -22,15 +24,18 @@ public class ContactController {
         return contactVO;
     }
 
-    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    @RequestMapping(value = "/ping", method = GET)
     public String ping() {
         LOGGER.info("--- Contact Service REST Controller ping method invoked. ---");
         return "Contact Service REST Controller";
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseStatus(CONFLICT)
     public String handleException(Exception e) {
         return e.getMessage();
     }
+
+//    produces = {"application/json", "application/xml"},
+//    consumes = {"application/json", "application/xml"}
 }
