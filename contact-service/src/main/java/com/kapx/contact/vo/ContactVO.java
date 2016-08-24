465cafd336d3a3c2ef7191ea041910c1d268bd0c
KAPIL <xkapil@gmail.com>
2016-08-24 17:16:05 +0100

    lombok dependency added

--- contact-service/src/main/java/com/kapx/contact/vo/ContactVO.java
+++ contact-service/src/main/java/com/kapx/contact/vo/ContactVO.java
@@ -1,64 +1,13 @@
 package com.kapx.contact.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ContactVO {
     private String firstName;
     private String lastName;
     private String phone;
     private String email;
-
-    public ContactVO() {}
-
-    public ContactVO(final ContactBuilder contactBuilder) {
-        this.firstName = contactBuilder.firstName;
-        this.lastName = contactBuilder.lastName;
-        this.phone = contactBuilder.phone;
-        this.email = contactBuilder.email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public static class ContactBuilder {
-        private String firstName;
-        private String lastName;
-        private String phone;
-        private String email;
-
-        public ContactBuilder withFirstName(final String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public ContactBuilder withLastName(final String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public ContactBuilder withPhone(final String phone) {
-            this.phone = phone;
-            return this;
-        }
-
-        public ContactBuilder withEmail(final String email) {
-            this.email = email;
-            return this;
-        }
-
-        public ContactVO build() {
-            return new ContactVO(this);
-        }
-    }
 }
