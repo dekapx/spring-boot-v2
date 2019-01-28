c1e6291d0517c8f1c561addf6f7f77be1b111279
KAPIL <kapil@KAPILs-MacBook-Pro.local>
2019-01-28 21:55:22 +0000

    command factory service location implmentation

--- spring-boot-audit/src/main/java/com/dekapx/springboot/command/CommandFactoryImpl.java
+++ spring-boot-audit/src/main/java/com/dekapx/springboot/command/CommandFactoryImpl.java
@@ -2,38 +2,26 @@ package com.dekapx.springboot.command;
 
 import com.dekapx.springboot.enums.CommandType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component("commandFactory")
 public class CommandFactoryImpl implements CommandFactory {
-    @Autowired
-    @Qualifier("saveCommand")
-    private Command saveCommand;
+
+    private final List<Command> commands;
 
     @Autowired
-    @Qualifier("authorizeCommand")
-    private Command authorizeCommand;
+    public CommandFactoryImpl(final List<Command> commands) {
+        this.commands = commands;
+    }
 
-    @Autowired
-    @Qualifier("updateCommand")
-    private Command updateCommand;
-
-    public Command getCommand(final CommandType commandType) {
-        Command command = null;
-        switch (commandType) {
-            case SAVE:
-                command = saveCommand;
-                break;
-            case AUTHORISE:
-                command = authorizeCommand;
-                break;
-            case UPDATE:
-                command = updateCommand;
-                break;
-            default:
-                throw new CommandNotFoundException("Command not found");
-        }
-        return command;
+    @Override
+    public Command getCommand(CommandType commandType) {
+        return commands
+                .stream()
+                .filter(command -> command.getCommandType().equals(commandType))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
---
+++ spring-boot-audit/src/main/java/com/dekapx/springboot/config/CommandConfig.java
@@ -0,0 +1,17 @@
+package com.dekapx.springboot.config;
+
+import com.dekapx.springboot.command.Command;
+import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommandConfig {
+    @Bean
+    public ServiceLocatorFactoryBean CommandFactoryServiceLocatorBean() {
+        final ServiceLocatorFactoryBean factoryBean = new ServiceLocatorFactoryBean();
+        factoryBean.setServiceLocatorInterface(Command.class);
+        return factoryBean;
+    }
+
+}
