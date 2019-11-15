b8367ee8387cb4074465d124b2a30a1c22a0f609
KAPIL <kapil@KAPILs-MacBook-Pro.local>
2019-11-15 18:05:32 +0000

    spring boot ssl example

---
+++ springboot-ssl-security/src/main/java/com/dekapx/springboot/controller/PingController.java
@@ -0,0 +1,27 @@
+package com.dekapx.springboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class PingController {
+    @GetMapping(value = "/ping", produces = "application/json")
+    public String home() {
+        log.info("PingController invoked...");
+        return "Hi! this URL is active...";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(CONFLICT)
+    public String handleException(final Exception e) {
+        return e.getMessage();
+    }
+}
