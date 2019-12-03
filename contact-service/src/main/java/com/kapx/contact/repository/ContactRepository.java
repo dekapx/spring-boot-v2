4b439abd67d4d438f0ad5e542929d8fecc9cbbac
KAPIL <kapil@KAPILs-MacBook-Pro.local>
2019-12-03 14:15:15 +0000

    cleanup for contact-service

--- contact-service/src/main/java/com/kapx/contact/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kapx.contact.repository;
-
-import com.kapx.contact.domain.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository<Contact,Long> {
-}
--- contact-service/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO CONTACT(FIRST_NAME,LAST_NAME,ADDRESS,PHONE,EMAIL) VALUES ('De','Kapx', '2121, El Comino Real, San Mateo, CA', '123456789', 'dekapx@kapx-inc.com');
