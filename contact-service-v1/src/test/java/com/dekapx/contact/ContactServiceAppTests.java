203f5e16dc7c5e8bb9603afd3d5b2f0e6a90862f
KAPIL <dekapx@MacBook-Pro.local>
2020-11-15 18:38:38 +0000

    controller added

--- contact-service-v1/src/test/java/com/dekapx/contact/ContactServiceAppTests.java
+++ contact-service-v1/src/test/java/com/dekapx/contact/ContactServiceAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ContactServiceV1ApplicationTests {
+class ContactServiceAppTests {
 
 	@Test
 	void contextLoads() {
