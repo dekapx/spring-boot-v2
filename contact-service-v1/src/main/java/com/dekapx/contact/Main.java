b6d9564564253e58b35b0c2a7a0d5032dda285f3
KAPIL <dekapx@MacBook-Pro.local>
2020-11-19 10:47:41 +0000

    code refactoring

--- contact-service-v1/src/main/java/com/dekapx/contact/Main.java
+++ contact-service-v1/src/main/java/com/dekapx/contact/Main.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactServiceApp {
+public class Main {
 	public static void main(String[] args) {
-		SpringApplication.run(ContactServiceApp.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 }
